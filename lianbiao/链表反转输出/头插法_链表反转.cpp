9a5de3d8d84dab44d65c70da25290c997699a46f
gaopei <gaopeigpgp@126.com>
2019-04-25 15:11:18 +0800

    头插法_链表

--- lianbiao/链表反转输出/头插法_链表反转.cpp
+++ lianbiao/链表反转输出/头插法_链表反转.cpp
@@ -1,13 +1,37 @@
-#include <stdio.h> 
-int main(void){
-	int i = 0;
-	int a[3] = {0};
-	for(i = 0;i <= 3;i++){
-		a[i] = 0;
-		printf("i:%d\n",&i);
-		printf("a:%d,i:%d\n",&a[i],i); 
-		printf("hello\n");
-	}
+/**
+* Á´±í·´×ª 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct LL{
+    int a;
+    struct LL *next;
+}L1;
+int main(){
+    L1 *phead = (L1 *)malloc(sizeof(L1));
+    L1 *p = phead;;
+    L1 *q;
+    int n = 3;
+    for (int i = 0;i < n;i++){
+        q = (L1 *) malloc(sizeof(L1));
+        scanf("%d",&q->a);
+        q->next = NULL;
+        p->next = q;
+        p = q;
+    }
+    q = phead->next;
+    printf("%d\n",q); 
+    phead = NULL;
+    while(q != NULL){
+        p = q;
+        q = q->next;
+        p->next = phead;
+        phead = p;
+    }
+    
+    while(phead != NULL){
+        printf("%d\n",phead->a);
+        phead = phead->next;
+    }
 }
-
-
